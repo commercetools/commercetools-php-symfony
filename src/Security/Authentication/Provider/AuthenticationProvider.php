cf4571737f39a642ea15046fb6563246367393e3
Jens Schulze <jens.schulze@commercetools.de>
2016-07-04 09:51:22 +0200

    refactor(AuthProvider): remove null values from session

--- src/Security/Authentication/Provider/AuthenticationProvider.php
+++ src/Security/Authentication/Provider/AuthenticationProvider.php
@@ -90,14 +90,13 @@ class AuthenticationProvider extends UserAuthenticationProvider
             }
             $user->setId($customer->getId());
             $this->session->set(CustomerRepository::CUSTOMER_ID, $customer->getId());
-            $this->session->set(
-                CartRepository::CART_ID,
-                $result->getCart() === null ? null : $result->getCart()->getId()
-            );
-            $this->session->set(
-                CartRepository::CART_ITEM_COUNT,
-                $result->getCart() === null ? null : $result->getCart()->getLineItemCount()
-            );
+            if (!is_null($result->getCart())) {
+                $this->session->set(CartRepository::CART_ID, $result->getCart()->getId());
+                $this->session->set(CartRepository::CART_ITEM_COUNT, $result->getCart()->getLineItemCount());
+            } else {
+                $this->session->remove(CartRepository::CART_ID);
+                $this->session->remove(CartRepository::CART_ITEM_COUNT);
+            }
         }
     }
 
