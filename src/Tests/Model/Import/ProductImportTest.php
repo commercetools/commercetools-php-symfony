08f8fc9b204cecf805b1f9d9ec1b99cc301b355e
Ibrahim Selim <ibrahim.abu-el-alaa@commercetools.de>
2016-12-29 16:55:13 +0100

    WIP : ProductImport Test and code refactor

---
+++ src/Tests/Model/Import/ProductImportTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ibrahimselim
+ * Date: 29/12/16
+ * Time: 11:58
+ */
+
+namespace Commercetools\Symfony\CtpBundle\Tests\Model\Import;
+
+
+class ProductImportTest
+{
+
+}
