7fca1291ebddff8be96f28a08ec47a3bc22b8545
Ibrahim Selim <ibrahim.abu-el-alaa@commercetools.de>
2017-01-26 17:51:38 +0100

    WIP (Order Import)

--- src/Model/Import/OrderData.php
+++ src/Model/Import/OrderData.php
@@ -12,9 +12,15 @@ use Commercetools\Core\Model\Cart\LineItemDraft;
 use Commercetools\Core\Model\Common\Address;
 use Commercetools\Core\Model\Common\Money;
 use Commercetools\Core\Model\Common\PriceDraft;
+use Commercetools\Core\Model\Common\TaxedPrice;
+use Commercetools\Core\Model\Common\TaxPortion;
 use Commercetools\Core\Model\Order\ProductVariantImportDraft;
+use Commercetools\Core\Client;
+use Commercetools\Core\Model\CustomerGroup\CustomerGroupCollection;
+use Commercetools\Core\Request\CustomerGroups\CustomerGroupQueryRequest;
+use Commercetools\Commons\Helper\QueryHelper;
 
-class OrderData
+class OrderData extends AbstractRequestBuilder
 {
     const ID ='id';
     const NAME ='name';
@@ -30,50 +36,109 @@ class OrderData
     const VALUE ='value';
     const PRODUCTID ='productId';
     const VARIANTID ='variantId';
+    const TOTALNET ='totalNet';
+    const TAXDPRICE ='taxedPrice';
+    const TOTALGROSS ='totalGross';
+    const TAXPORTIONS ='taxPortions';
+    const RATE ='rate';
+    const AMOUNT ='amount';
+    const COMPLETEDAT ='completedAt';
+    const CUSTOMERGROUP='customerGroup';
+
+    private $client;
+    private $customerGroups;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->customerGroups = $this->getCustomerGroups();
+    }
 
     public function mapOrderFromData($data)
     {
-        if (isset($data[self::TOTALPRICE])) {
-            $priceParts = explode(' ', $data[self::TOTALPRICE]);
-            $price[self::CURRENCYCODE] = $priceParts[0];
-            $price[self::CENTAMOUNT] = (int)$priceParts[1];
-            $data[self::TOTALPRICE] = $price;
-        }
-        if (isset($data[self::LINEITEMS])) {
-            if (isset($data [self::LINEITEMS][self::NAME])) {
-                unset($data[self::LINEITEMS][self::NAME]);
-            }
-            if (isset($data [self::LINEITEMS][self::VARIANT])) {
-                unset($data[self::LINEITEMS][self::VARIANT]);
-            }
+        foreach ($data as $key => $value) {
+            switch ($key) {
+                case self::TOTALNET:
+                    if (!empty($value) && $value !='') {
+                        $priceParts = explode(' ', $data[self::TOTALNET]);
+                        $price[self::CURRENCYCODE] = $priceParts[0];
+                        $price[self::CENTAMOUNT] = (int)$priceParts[1];
+                        $data[self::TOTALNET] = $price;
 
-            foreach ($data[self::LINEITEMS] as &$lineItem) {
-                if (isset($lineItem[self::PRICE])) {
-                    $priceParts = explode(' ', $lineItem[self::PRICE]);
+                        $priceParts = explode(' ', $data[self::TOTALGROSS]);
+                        $price[self::CURRENCYCODE] = $priceParts[0];
+                        $price[self::CENTAMOUNT] = (int)$priceParts[1];
+                        $data[self::TOTALGROSS] = $price;
+
+                        $data[self::TAXDPRICE][self::TOTALNET] = $data[self::TOTALNET];
+                        $data[self::TAXDPRICE][self::TOTALGROSS] = $data[self::TOTALGROSS];
+
+                        $priceParts = explode(' ', $data[self::TAXPORTIONS][self::AMOUNT]);
+                        $price[self::CURRENCYCODE] = $priceParts[0];
+                        $price[self::CENTAMOUNT] = (int)$priceParts[1];
+                        $data[self::TAXPORTIONS][self::AMOUNT] = $price;
+
+                        $data[self::TAXDPRICE][self::TAXPORTIONS][self::NAME] = $data[self::TAXPORTIONS][self::NAME];
+                        $data[self::TAXDPRICE][self::TAXPORTIONS][self::RATE] = (int)$data[self::TAXPORTIONS][self::RATE];
+                        $data[self::TAXDPRICE][self::TAXPORTIONS][self::AMOUNT] = $data[self::TAXPORTIONS][self::AMOUNT];
+                    }
+                    break;
+                case self::TOTALPRICE:
+                    $priceParts = explode(' ', $data[self::TOTALPRICE]);
                     $price[self::CURRENCYCODE] = $priceParts[0];
                     $price[self::CENTAMOUNT] = (int)$priceParts[1];
-                    $lineItem[self::PRICE] = [self::VALUE=>$price];
-                }
-                if (isset($lineItem[self::QUANTITY])) {
-                    $lineItem[self::QUANTITY] = (int) $lineItem[self::QUANTITY];
-                }
-                if (isset($lineItem[self::PRODUCTID]) &&  !isset($lineItem[self::VARIANTID])) {
-                    unset($lineItem[self::PRODUCTID]);
-                }
-                if (isset($lineItem[self::LINEITEMS])) {
-                    unset($lineItem[self::LINEITEMS]);
-                }
-                if (isset($lineItem[self::ID])) {
-                    unset($lineItem[self::ID]);
-                }
+                    $data[self::TOTALPRICE] = $price;
+                    break;
+                case self::BILLINGADDRESS:
+                    if (empty($data[self::BILLINGADDRESS])) {
+                        unset($data[self::BILLINGADDRESS]);
+                    }
+                    break;
+                case self::SHIPPINGADDRESS:
+                    if (empty($data[self::SHIPPINGADDRESS])) {
+                        unset($data[self::SHIPPINGADDRESS]);
+                    }
+                    break;
+                case self::LINEITEMS:
+                    if (isset($data[self::LINEITEMS][self::NAME])) {
+                          unset($data[self::LINEITEMS][self::NAME]);
+                    }
+                    if (isset($data[self::LINEITEMS][self::VARIANT])) {
+                        unset($data[self::LINEITEMS][self::VARIANT]);
+                    }
+                    foreach ($data[self::LINEITEMS] as &$lineItem) {
+                        if (isset($lineItem[self::PRICE])) {
+                            $priceParts = explode(' ', $lineItem[self::PRICE]);
+                            $price[self::CURRENCYCODE] = $priceParts[0];
+                            $price[self::CENTAMOUNT] = (int)$priceParts[1];
+                            $lineItem[self::PRICE] = [self::VALUE => $price];
+                        }
+                        if (isset($lineItem[self::QUANTITY])) {
+                            $lineItem[self::QUANTITY] = (int)$lineItem[self::QUANTITY];
+                        }
+                        if (isset($lineItem[self::PRODUCTID]) && !isset($lineItem[self::VARIANT][self::VARIANTID])) {
+                            unset($lineItem[self::PRODUCTID]);
+                        }
+                        if (isset($lineItem[self::LINEITEMS])) {
+                            unset($lineItem[self::LINEITEMS]);
+                        }
+                        if (isset($lineItem[self::ID])) {
+                            unset($lineItem[self::ID]);
+                        }
+                    }
+                    break;
             }
         }
         return $data;
     }
     public function getOrderObjsFromArr($OrderArr)
     {
-        if (isset($OrderArr[self::TOTALPRICE])) {
-            $OrderArr[self::TOTALPRICE] = Money::fromArray($OrderArr[self::TOTALPRICE]);
+        if (isset($OrderArr[self::TAXDPRICE])) {
+            $OrderArr[self::TAXDPRICE][self::TOTALNET] = Money::fromArray($OrderArr[self::TAXDPRICE][self::TOTALNET]);
+            $OrderArr[self::TAXDPRICE][self::TOTALGROSS] = Money::fromArray($OrderArr[self::TAXDPRICE][self::TOTALGROSS]);
+            $OrderArr[self::TAXDPRICE][self::TAXPORTIONS][self::AMOUNT] = Money::fromArray($OrderArr[self::TAXDPRICE][self::TAXPORTIONS][self::AMOUNT]);
+            $OrderArr[self::TAXDPRICE][self::TAXPORTIONS] =[TaxPortion::fromArray($OrderArr[self::TAXDPRICE][self::TAXPORTIONS])];
+            $OrderArr[self::TAXDPRICE] = TaxedPrice::fromArray($OrderArr[self::TAXDPRICE]);
         }
         if (isset($OrderArr[self::LINEITEMS])) {
             foreach ($OrderArr[self::LINEITEMS] as &$lineItem) {
@@ -88,14 +153,35 @@ class OrderData
         }
         if (isset($OrderArr[self::BILLINGADDRESS]) && !empty($OrderArr[self::BILLINGADDRESS])) {
             $OrderArr[self::BILLINGADDRESS] = Address::fromArray($OrderArr[self::BILLINGADDRESS]);
-        } elseif (isset($OrderArr[self::BILLINGADDRESS])) {
-            unset($OrderArr[self::BILLINGADDRESS]);
         }
         if (isset($OrderArr[self::SHIPPINGADDRESS]) && !empty($OrderArr[self::SHIPPINGADDRESS])) {
             $OrderArr[self::SHIPPINGADDRESS] = Address::fromArray($OrderArr[self::SHIPPINGADDRESS]);
-        } elseif (isset($OrderArr[self::SHIPPINGADDRESS])) {
-            unset($OrderArr[self::SHIPPINGADDRESS]);
+        }
+        if (isset($OrderArr[self::CUSTOMERGROUP]) && !empty($OrderArr[self::CUSTOMERGROUP])) {
+            $OrderArr[self::CUSTOMERGROUP] = $this->customerGroups[$OrderArr[self::CUSTOMERGROUP]];
         }
         return $OrderArr;
+    }
+
+    public function getOrderItemsToChange($orderDataArray, $order)
+    {
+        $intersect = $this->arrayIntersectRecursive($order, $orderDataArray);
+        $toChange = $this->arrayDiffRecursive($orderDataArray, $intersect);
+        return $toChange;
+    }
+
+    private function getCustomerGroups()
+    {
+        $request = CustomerGroupQueryRequest::of();
+        $helper = new QueryHelper();
+        $customerGroups = $helper->getAll($this->client, $request);
+        /**
+         * @var CustomerGroupCollection $customerGroups ;
+         */
+        $customerGroupsByName = [];
+        foreach ($customerGroups as $customerGroup) {
+            $customerGroupsByName[$customerGroup->getName()] = $customerGroup->getReference();
+        }
+        return $customerGroupsByName;
     }
 }
--- src/Model/Import/OrdersRequestBuilder.php
+++ src/Model/Import/OrdersRequestBuilder.php
@@ -9,21 +9,15 @@
 namespace Commercetools\Symfony\CtpBundle\Model\Import;
 
 use Commercetools\Core\Client;
-use Commercetools\Core\Model\Cart\LineItemDraft;
-use Commercetools\Core\Model\Cart\LineItemDraftCollection;
-use Commercetools\Core\Model\Common\Address;
-use Commercetools\Core\Model\Common\Money;
-use Commercetools\Core\Model\Common\PriceDraft;
 use Commercetools\Core\Model\Order\ImportOrder;
-use Commercetools\Core\Model\Order\LineItemImportDraft;
 use Commercetools\Core\Model\Order\Order;
-use Commercetools\Core\Model\Order\ProductVariantImportDraft;
 use Commercetools\Core\Request\Orders\OrderImportRequest;
 use Commercetools\Core\Request\Orders\OrderQueryRequest;
 
 class OrdersRequestBuilder extends AbstractRequestBuilder
 {
     const ID ='id';
+    const VERSION ='version';
     const NAME ='name';
     const VARIANT ='variant';
     const LINEITEMS ='lineItems';
@@ -37,6 +31,9 @@ class OrdersRequestBuilder extends AbstractRequestBuilder
     const VALUE ='value';
     const PRODUCTID ='productId';
     const VARIANTID ='variantId';
+    const ORDERSTATE ='orderState';
+    const SHIPMENTSTATE ='shipmentState';
+    const PAYMENTSTATE ='paymentState';
 
     private $client;
     private $orderDataObj;
@@ -44,7 +41,7 @@ class OrdersRequestBuilder extends AbstractRequestBuilder
     public function __construct(Client $client)
     {
         $this->client = $client;
-        $this->orderDataObj= new OrderData();
+        $this->orderDataObj= new OrderData($client);
     }
 
     private function getOrdersByIdentifiedByColumn($orders, $identifiedByColumn)
@@ -88,8 +85,8 @@ class OrdersRequestBuilder extends AbstractRequestBuilder
                     $this->getIdentifierFromArray($identifiedByColumn, $ordersData)
                 )
             )
-
             ->limit(500);
+
         $response = $request->executeWithClient($this->client);
         $orders = $request->mapFromResponse($response);
 
@@ -101,7 +98,7 @@ class OrdersRequestBuilder extends AbstractRequestBuilder
         foreach ($ordersDataArr as $key => $orderData) {
             if (isset($ordersArr[$key])) {
 //                $order = $ordersArr[$key];
-//                $request = $this->getUpdateRequest($order, $orderData);
+//                $request = $this->getUpdateRequest($orderData, $order);
 //                if (!$request->hasActions()) {
 //                    $request = null;
 //                }
@@ -114,11 +111,44 @@ class OrdersRequestBuilder extends AbstractRequestBuilder
         return $requests;
     }
 
+//    private function getUpdateRequestsToChange($toChange)
+//    {
+//        $actions=[];
+//        foreach ($toChange as $heading => $data) {
+//            switch ($heading) {
+//                case self::ORDERSTATE:
+//                    $actions[$heading] = OrderChangeOrderStateAction::ofOrderState($data);
+//                    break;
+//                case self::SHIPMENTSTATE:
+//                    $actions[$heading] = OrderChangeShipmentStateAction::ofShipmentState($data);
+//                    break;
+//                case self::PAYMENTSTATE:
+//                    $actions[$heading] = OrderChangePaymentStateAction::ofPaymentState($data);
+//                    break;
+//            }
+//        }
+//        return $actions;
+//    }
+//    private function getUpdateRequest($orderDataArray, Order $order)
+//    {
+//        $orderDataArray= $this->orderDataObj->mapOrderFromData($orderDataArray);
+//        $order = $order->toArray();
+//
+//        $toChange = $this->orderDataObj->getOrderItemsToChange($orderDataArray, $order);
+//
+//        $actions=[];
+//        $actions = array_merge_recursive($actions, $this->getUpdateRequestsToChange($toChange));
+//
+//        $request = OrderUpdateRequest::ofIdAndVersion($order[self::ID], $order[self::VERSION]);
+//        $request->setActions($actions);
+//    //        print_r((string)$request->httpRequest()->getBody());
+//        return $request;
+//    }
+
     private function getCreateRequest($orderDataArray)
     {
         $orderDataArray= $this->orderDataObj->mapOrderFromData($orderDataArray);
         $orderDataobj= $this->orderDataObj->getOrderObjsFromArr($orderDataArray);
-
         $order = ImportOrder::fromArray($orderDataobj);
         $request = OrderImportRequest::ofImportOrder($order);
         return $request;
