0456912f25626b7873e3074fce7d1a3191e21400
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-05-08 18:16:35 +0200

    WIP: customer bundle tests

--- src/CustomerBundle/Tests/Event/CustomerPostUpdateEventTest.php
+++ src/CustomerBundle/Tests/Event/CustomerPostUpdateEventTest.php
@@ -6,7 +6,24 @@
 namespace Commercetools\Symfony\CustomerBundle\Tests\Event;
 
 
+use Commercetools\Core\Model\Customer\Customer;
+use Commercetools\Symfony\CustomerBundle\Event\CustomerPostUpdateEvent;
+
 class CustomerPostUpdateEventTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCustomerPostUpdateEvent()
+    {
+        $customer = $this->prophesize(Customer::class);
+        $action = $this->prophesize(CustomerSetFirstNameAction::class);
+        $secondCustomer = $this->prophesize(Customer::class);
 
+        $postUpdateEvent = new CustomerPostUpdateEvent($customer->reveal(), [$action->reveal()]);
+        $postUpdateEvent->setCustomer($secondCustomer->reveal());
+
+        $this->assertNotSame($customer->reveal(),$secondCustomer->reveal());
+        $this->assertSame($secondCustomer->reveal(), $postUpdateEvent->getCustomer());
+        $this->assertNotSame($customer->reveal(), $postUpdateEvent->getCustomer());
+
+        $this->assertEquals([$action->reveal()], $postUpdateEvent->getActions());
+    }
 }
