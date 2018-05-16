4ec2ef2736a5acf3d43a5f15d29146109a3bc938
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-05-16 16:18:08 +0200

    WIP: cart_bundle CartGetEvent changes

--- src/CartBundle/Event/CartGetEvent.php
+++ src/CartBundle/Event/CartGetEvent.php
@@ -6,9 +6,23 @@
 namespace Commercetools\Symfony\CartBundle\Event;
 
 
+use Commercetools\Core\Model\Cart\Cart;
 use Symfony\Component\EventDispatcher\Event;
 
 class CartGetEvent extends Event
 {
+    private $cart;
 
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
+
+    /**
+     * @return Cart
+     */
+    public function getCart()
+    {
+        return $this->cart;
+    }
 }
