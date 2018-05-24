52b39f7bde768297ba36614b173e236a8f4cc7e1
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-05-24 14:59:01 +0200

    WIP: review_bundle create review, get reviews

--- src/ReviewBundle/Model/Repository/ReviewRepository.php
+++ src/ReviewBundle/Model/Repository/ReviewRepository.php
@@ -6,6 +6,7 @@ namespace Commercetools\Symfony\ReviewBundle\Model\Repository;
 
 use Commercetools\Core\Builder\Request\RequestBuilder;
 use Commercetools\Core\Model\Review\Review;
+use Commercetools\Core\Model\Review\ReviewDraft;
 use Commercetools\Symfony\CtpBundle\Model\QueryParams;
 use Commercetools\Symfony\CtpBundle\Service\MapperFactory;
 use Commercetools\Symfony\CtpBundle\Model\Repository;
@@ -28,6 +29,27 @@ class ReviewRepository extends Repository
         $request = RequestBuilder::of()->reviews()->getById($reviewId);
 
         return $this->executeRequest($request, $locale, $params);
+    }
+
+    public function getReviewsByProductId($locale, $productId, QueryParams $params = null)
+    {
+        $predicate = 'target(id = "' . $productId . '") and target(typeId="product")';
+        $request = RequestBuilder::of()->reviews()->query()->where($predicate);
+
+        return $this->executeRequest($request, $locale, $params);
+    }
+
+    public function createReviewForProduct($locale, $productReference, $customerReference, $text, $rating)
+    {
+        $reviewDraft = ReviewDraft::of()
+            ->setText($text)
+            ->setRating($rating)
+            ->setCustomer($customerReference)
+            ->setTarget($productReference)
+            ->setLocale($locale);
+        $request = RequestBuilder::of()->reviews()->create($reviewDraft);
+
+        return $this->executeRequest($request, $locale);
     }
 
     public function update(Review $review, array $actions, QueryParams $params = null)
