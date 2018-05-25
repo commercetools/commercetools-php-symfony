b627cf4e7083f639bfa2c2692fa3d178de6ad522
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-05-25 17:31:19 +0200

    WIP: move leftovers from CTP_bundle to their respectives

--- src/ExampleBundle/Model/Form/Type/AddressType.php
+++ src/ExampleBundle/Model/Form/Type/AddressType.php
@@ -6,18 +6,13 @@
 namespace Commercetools\Symfony\ExampleBundle\Model\Form\Type;
 
 
-use Commercetools\Symfony\ExampleBundle\Entity\UserAddress;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Choice;
 
 class AddressType extends AbstractType
 {
