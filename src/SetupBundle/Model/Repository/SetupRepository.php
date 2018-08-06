b91a84e1cc81ff5b58176c140cbbf89e20d8c865
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-08-06 17:11:30 +0200

    WIP: remove actions for channels

--- src/SetupBundle/Model/Repository/SetupRepository.php
+++ src/SetupBundle/Model/Repository/SetupRepository.php
@@ -8,7 +8,6 @@ namespace Commercetools\Symfony\SetupBundle\Model\Repository;
 
 use Commercetools\Core\Builder\Request\RequestBuilder;
 use Commercetools\Core\Client;
-use Commercetools\Core\Model\Channel\Channel;
 use Commercetools\Core\Model\Project\Project;
 use Commercetools\Symfony\CtpBundle\Logger\Logger;
 use Commercetools\Symfony\CtpBundle\Model\Repository;
@@ -39,38 +38,6 @@ class SetupRepository extends Repository
         $updateRequest = RequestBuilder::of()->project()->update($project)->setActions($actions);
 
         return $this->executeRequest($updateRequest);
-    }
-
-    public function getChannels($condition = null)
-    {
-        $request = RequestBuilder::of()->channels()->query();
-
-        if (!is_null($condition)) {
-            $request->where($condition);
-        }
-
-        return $this->executeRequest($request);
-    }
-
-    public function createChannel($channelDraft)
-    {
-        $request = RequestBuilder::of()->channels()->create($channelDraft);
-
-        return $this->executeRequest($request);
-    }
-
-    public function updateChannel(Channel $channel, array $actions)
-    {
-        $updateRequest = RequestBuilder::of()->channels()->update($channel)->setActions($actions);
-
-        return $this->executeRequest($updateRequest);
-    }
-
-    public function deleteChannel(Channel $channel)
-    {
-        $deleteRequest = RequestBuilder::of()->channels()->delete($channel);
-
-        return $this->executeRequest($deleteRequest);
     }
 
     public function applyConfiguration($config, $online)
