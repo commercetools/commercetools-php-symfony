a6741d7599d596b76945428998fa4435925d91e4
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-08-31 11:15:49 +0200

    WIP: state_bundle,  fix supported class ItemStateWrapper

--- src/StateBundle/Model/StateType.php
+++ src/StateBundle/Model/StateType.php
@@ -6,11 +6,8 @@
 namespace Commercetools\Symfony\StateBundle\Model;
 
 
-use Commercetools\Core\Model\Cart\CustomLineItem;
-use Commercetools\Core\Model\Cart\LineItem;
-use Commercetools\Core\Model\Order\ItemState;
 use Commercetools\Core\Model\Order\Order;
-use Commercetools\Core\Model\Payment\Payment;
+use Commercetools\Core\Model\Payment\PaymentStatus;
 use Commercetools\Core\Model\Product\Product;
 use Commercetools\Core\Model\Review\Review;
 
@@ -18,13 +15,9 @@ class StateType
 {
     const TYPES = [
         'OrderState' => [Order::class],
-        'LineItemState' => [
-            ItemState::class,
-            LineItem::class,
-            CustomLineItem::class
-        ],
+        'LineItemState' => [ItemStateWrapper::class],
         'ProductState' => [Product::class],
         'ReviewState' => [Review::class],
-        'PaymentState' => [Payment::class]
+        'PaymentState' => [PaymentStatus::class]
     ];
 }
