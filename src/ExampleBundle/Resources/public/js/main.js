54084ad968ff23b6facd9eb77d4b1d57bbd78ce0
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2019-07-25 14:19:30 +0200

    WIP: fixes for mini cart & search form

--- src/ExampleBundle/Resources/public/js/main.js
+++ src/ExampleBundle/Resources/public/js/main.js
@@ -562,3 +562,128 @@ jQuery(document).on('submit', '.add-review-form', function(e){
     }
   });
 });
+
+
+
+/**
+ * js from old version
+ */
+
+var required = false;
+
+function debounce(fn, delay) {
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
+
+
+jQuery(document).ready(function($){
+  //Add class to submitbutton form
+  $('#form_Submit').addClass('btn btn-primary spacer');
+
+  // Clear form function
+  $("#defaultCheck").on("click",function(){
+    var clear = confirm('Are you sure?');
+
+    if (clear == true){
+      $("[id^='form_shippingAddress_']").val("");
+      $("[id^='form_billingAddress_']").val("");
+    }
+  });
+
+  // disable autocomplete
+  $("#form-search #search-input").attr("autocomplete", "off");
+
+  $("#form_check").on("click",function(){
+    if ($("#form_check:checked").val()) {
+      $("#form_billingAddress").fadeOut();
+    } else {
+      $("#form_billingAddress").fadeIn();
+    }
+    $("#form_billingAddress input[data-required='true']").attr('required',required);
+    required = !required;
+  });
+
+  $("#form_check").on("click",function(){
+    $("[id^='form_shippingAddress_']").each(function(){
+      var id = $(this).attr("id").replace("shipping","billing"),
+          $country = $("#form_shippingAddress_country").val(),
+          $salutation = $("#form_shippingAddress_salutation").val();
+      if ($("#"+id).val() == '') {
+        $("#"+id).val($(this).val());
+      }
+      $('#form_billingAddress_country').val($country);
+      $('#form_billingAddress_salutation').val($salutation);
+    });
+  });
+
+  // animate the search results container
+  $("#form-search #search-input").keypress(debounce(function () {
+    $(".search-container").css("display", "block");
+
+  }, 1));
+
+  // ajax handling on search
+  $("#form-search #search-input").on("keyup", debounce(function (event) {
+    var searchTerm = $(event.target).val();
+
+    if(searchTerm.length <= 0) return;
+
+    var productUrl = '/product/slug/';
+    var itemUrl = '/suggest/';
+    var col4 = '<div class="col-md-4">';
+
+    $("#search-results").html("");
+
+    $.ajax({
+      type: 'POST',
+      url: itemUrl + searchTerm,
+      dataType: "json"
+    }).done(function (data) {
+      if(data) {
+        for (i in data) {
+          // build founded items
+          var img = '<img class="ctp-img" src="' + data[i].image + '" alt="' + data[i].name + '"/>';
+          var price = '<p>' + data[i].price + '</p>';
+          var product = col4+'<a href="' + productUrl + data[i].link + '"><div><h3>' + data[i].name + '</h3>' + img + '<p>' + data[i].desc + '</p>'+ price +'</div></a></div>';
+          $("#search-results").append(product);
+        }
+      }
+
+      var jsonData = data.toString();
+
+      if (jsonData.length <= 0){
+        setTimeout(function emptyResult(){
+          var msg = '<div class="empty-result"><h3>No result</h3><p>Try a different search query</p></div>';
+          $("#search-results").append(msg);
+        }, 400);
+      }
+
+      // activate the loading gif
+      $(document).on({
+        ajaxStart: function() {
+          $(".loading").css("display", "block");
+        },
+        ajaxStop: function() {
+          $(".loading").css("display", "none");
+        }
+      });
+
+      //check for input
+      $("#form-search #search-input").on("keyup", function(){
+        if( $(this).val().length <= 0 ) {
+          $(".search-container").css("display", "none");
+        }
+      });
+
+    });
+
+  }, 300));//time ajax request
+
+});
