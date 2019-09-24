c35ece4aeb4793f4e318dac05ec58aaf3b102da1
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2019-09-24 12:51:07 +0200

    WIP: move me-client to CtpBundle

--- src/CartBundle/Manager/CartManagerInterface.php
+++ src/CartBundle/Manager/CartManagerInterface.php
@@ -5,7 +5,45 @@
 
 namespace Commercetools\Symfony\CartBundle\Manager;
 
+use Commercetools\Core\Model\Cart\Cart;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Symfony\CartBundle\Model\CartUpdateBuilder;
+
 interface CartManagerInterface
 {
+    /**
+     * @param Cart $cart
+     * @return CartUpdateBuilder
+     */
+    public function update(Cart $cart);
 
+
+    /**
+     * @param Cart $cart
+     * @param AbstractAction $action
+     * @param string|null $eventName
+     * @return AbstractAction[]
+     */
+    public function dispatch(Cart $cart, AbstractAction $action, $eventName = null);
+
+
+    /**
+     * @param Cart $cart
+     * @param array $actions
+     * @return Cart
+     */
+    public function apply(Cart $cart, array $actions);
+
+    /**
+     * @param Cart $cart
+     * @param array $actions
+     * @return AbstractAction[]
+     */
+    public function dispatchPostUpdate(Cart $cart, array $actions);
+
+
+    /**
+     * @param Cart|null $cart
+     */
+    public function dispatchPostGet(Cart $cart = null);
 }
