983fa6e74fe8d83a41ece3ac8683ccaf8456cacd
Jens Schulze <jenschude@gmail.com>
2019-11-22 11:11:36 +0100

    refactor services to use SDK client factories and inject middlewares using config

--- src/CtpBundle/Service/ClientFactory.php
+++ src/CtpBundle/Service/ClientFactory.php
@@ -5,62 +5,14 @@
 
 namespace Commercetools\Symfony\CtpBundle\Service;
 
-use Commercetools\Core\Client\ApiClient;
-use Commercetools\Core\Client\ClientFactory as CtpClientFactory;
 use Commercetools\Core\Config;
-use Commercetools\Core\Model\Common\Context;
-use Commercetools\Symfony\CtpBundle\Profiler\CommercetoolsProfilerExtension;
-use Psr\Log\LoggerInterface;
+use Commercetools\Core\Client\ClientFactory as CtpClientFactory;
+use GuzzleHttp\Client;
 
 class ClientFactory
 {
-    private $contextFactory;
-    private $cache;
-    private $logger;
-    private $converter;
-    private $config;
-    private $profiler;
-
-    public function __construct(
-        Config $config,
-        ContextFactory $contextFactory,
-        $cache,
-        LocaleConverter $converter,
-        LoggerInterface $logger = null,
-        CommercetoolsProfilerExtension $profiler = null
-    ) {
-        $this->config = $config;
-        $this->contextFactory = $contextFactory;
-        $this->cache = $cache;
-        $this->converter = $converter;
-        $this->logger = $logger;
-        $this->profiler = $profiler;
-    }
-
-    /**
-     * @param string $locale
-     * @param Context $context
-     * @param Config|array $config
-     * @return ApiClient
-     */
-    public function build(
-        $locale = null,
-        Context $context = null,
-        $config = null
-    ) {
-        if (is_array($config)) {
-            $config = Config::fromArray($config);
-        }
-        if (is_null($config)) {
-            $config = $this->config;
-        }
-        if (is_null($context)) {
-            $context = $this->contextFactory->build($locale);
-        }
-        $config->setContext($context);
-
-        $client = CtpClientFactory::of()->createClient($config, $this->logger, $this->cache, null, null, $context);
-
-        return $client;
+    public function buildOauthClient(CtpClientFactory $factory, Config $config): Client
+    {
+        return $factory->createAuthClient($config->getOAuthClientOptions(), $config->getCorrelationIdProvider());
     }
 }
--- src/CtpBundle/Service/ConfigFactory.php
+++ src/CtpBundle/Service/ConfigFactory.php
@@ -6,23 +6,44 @@
 namespace Commercetools\Symfony\CtpBundle\Service;
 
 use Commercetools\Core\Config;
-use GuzzleHttp\HandlerStack;
+use Commercetools\Symfony\CtpBundle\CtpBundle;
+use Commercetools\Symfony\CtpBundle\Profiler\CommercetoolsProfilerExtension;
 use GuzzleHttp\MessageFormatter;
 use GuzzleHttp\Middleware;
+use Psr\Http\Message\RequestInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class ConfigFactory
 {
-    public static function create(array $config, HandlerStack $handlerStack, LoggerInterface $logger, $debug = false)
+    public static function create(array $config, LoggerInterface $logger, CommercetoolsProfilerExtension $profiler = null, $debug = false)
     {
         $config = Config::fromArray($config);
 
-        $config->setClientOptions(['handler' => $handlerStack]);
+        $clientOptions = $config->getClientOptions();
+        $middleWares = $clientOptions['middlewares'] ?? [];
+
+        $middleWares['useragent'] = Middleware::mapRequest(
+            function (RequestInterface $request) {
+                return $request->withHeader('User-Agent', $request->getHeaderLine('User-Agent') . " ctp-bundle/" . CtpBundle::VERSION . " (symfony/" . Kernel::VERSION . ")");
+            });
+
+        if ($profiler instanceof CommercetoolsProfilerExtension) {
+            $middleWares['profiler'] = $profiler->getProfileMiddleWare();
+        }
+
+        $clientOptions['middlewares'] = $middleWares;
+        $config->setClientOptions($clientOptions);
 
         if ($debug) {
-            $handler = HandlerStack::create();
-            $handler->push(Middleware::log($logger, new MessageFormatter()));
-            $config->setOAuthClientOptions(['handler' => $handler]);
+            $oauthOptions = $config->getOAuthClientOptions();
+            $oauthMiddleWares = $oauthOptions['middlewares'] ?? [];
+            $oauthMiddleWares['ctp_log'] = Middleware::log($logger, new MessageFormatter());
+            if ($profiler instanceof CommercetoolsProfilerExtension) {
+                $oauthMiddleWares['profiler'] = $profiler->getProfileMiddleWare();
+            }
+            $oauthOptions['middlewares'] = $oauthMiddleWares;
+            $config->setOAuthClientOptions($oauthOptions);
         }
 
         return $config;
